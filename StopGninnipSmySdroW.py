5ad9dc5695102fb3cfab4d58eec4d594c345dc61
DiscordantST <discordantstas@gmail.com>
2022-11-26 20:55:10 +0300

    #solution StopGninnipSmySdroW.py

--- StopGninnipSmySdroW.py
+++ StopGninnipSmySdroW.py
@@ -1,3 +1,9 @@
 def spin_words(sentence):
-    # Your code goes here
-    return None
+    d = []
+    for i in range(int(len(sentence.split()))):
+        if len(sentence.split()[i]) < 5:
+            d.append(sentence.split()[i])
+        if len(sentence.split()[i]) >= 5:
+            d.append(sentence.split()[i][::-1])
+    return " ".join(d)
+
