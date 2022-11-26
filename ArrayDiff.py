047121720f0e81fd4833261ef46fd44af6e2d60a
DiscordantST <discordantstas@gmail.com>
2022-11-27 00:17:49 +0300

    #solution ArrayDiff.py

--- ArrayDiff.py
+++ ArrayDiff.py
@@ -1,2 +1,14 @@
 def array_diff(a, b):
-    #your code here
+        return [x for x in a if x not in b]
+
+
+
+# #another answer
+# def array_diff(a, b):
+#     for item in b:
+#         while item in a:
+#             a.remove(item)
+#         print(a)
+
+
+
