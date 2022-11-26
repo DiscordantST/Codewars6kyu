a4582cf36c254759ef18f80dc7654b998deccc95
DiscordantST <discordantstas@gmail.com>
2022-11-26 18:49:08 +0300

    #solution MultiplesOf3Or5.py

--- MultiplesOf3Or5.py
+++ MultiplesOf3Or5.py
@@ -1,2 +1,11 @@
 def solution(number):
-    pass
+    sum = 0
+    for i in range(number):
+        if (i % 3 == 0) or (i % 5 == 0):
+            sum += i
+    return sum
+
+
+# one line answer:
+# return sum(x for x in range(number) if x % 3 == 0 or x % 5 == 0)
+
