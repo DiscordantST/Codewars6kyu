3f68577a5cac303665a7941927d272b70d5e321d
DiscordantST <discordantstas@gmail.com>
2022-11-27 19:35:31 +0300

    #solution ReplaceWithAlphabetPosition.py

--- ReplaceWithAlphabetPosition.py
+++ ReplaceWithAlphabetPosition.py
@@ -1,2 +1,8 @@
 def alphabet_position(text):
-    pass
+    simple_dict = dict(zip('abcdefghijklmnopqrstuvwxyz', range(1, 27)))
+    c = []
+    text1 = text.lower()
+    for i in range(len(text1)):
+        c.append(simple_dict.get(text1[i]))
+    return " ".join(map(str, [c for c in c if c != None]))
+
