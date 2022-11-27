6e365757cd95d31c4a4184092f0c5760b25f5f26
DiscordantST <discordantstas@gmail.com>
2022-11-27 21:39:38 +0300

    #solution PersistentBugger.py

--- PersistentBugger.py
+++ PersistentBugger.py
@@ -1,2 +1,10 @@
 def persistence(n):
-    pass
+    simple_list = []
+    number_of_repetitions = 0
+    while len(str(n)) >= 2:
+        for i in range(len(str(n))):
+            simple_list.append(int(str(n)[i]))
+        n = eval(str(simple_list)[1:-1].replace(',', '*'))
+        simple_list.clear()
+        number_of_repetitions += 1
+    return number_of_repetitions
