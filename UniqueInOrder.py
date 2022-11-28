7e36233dcdadb7a043e7f67b1d535fbb96464431
DiscordantST <discordantstas@gmail.com>
2022-11-28 17:18:12 +0300

    #solution UniqueInOrder.py

--- UniqueInOrder.py
+++ UniqueInOrder.py
@@ -1,2 +1,8 @@
 def unique_in_order(iterable):
-    pass
+    order_list = []
+    character = None
+    for i in iterable[0:]:
+        if i != character:
+            order_list.append(i)
+            character = i
+    return order_list
