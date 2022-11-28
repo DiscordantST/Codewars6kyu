4fab1223b303e31ce8e803958359486b615c8042
DiscordantST <discordantstas@gmail.com>
2022-11-28 13:31:18 +0300

    #solution TribonacciSequence.py

--- TribonacciSequence.py
+++ TribonacciSequence.py
@@ -1,2 +1,14 @@
 def tribonacci(signature, n):
-    pass
+    signature_sum = []
+    i = 0
+    while len(signature_sum) < n:
+        i = sum(signature[:])
+        signature.append(i)
+        signature_sum.append(signature.pop(0))
+    return signature_sum
+
+
+
+
+
+
